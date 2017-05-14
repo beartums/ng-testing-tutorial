cf99337fb502529998d373b837cdf99fd6bdcda0
Eric Griffith <eric@griffithnet.com>
2017-05-14 09:39:39 +0300

    First Tests using hashtables

--- tests/CreditCalculationService.spec.js
+++ tests/CreditCalculationService.spec.js
@@ -49,43 +49,39 @@ describe('CreditCalculationService', function() {
 	});
 
 	it('calculates simple one-input bundle price correctly', function() {
-		let mockClientBundle = {
-			BundleQuantity: 3,
-			Bundle: {
-				BundleCost_Fixed: 500,	BundleCost_PerBundle: 200,
-				BundleInputs: [
+		let mockCreditCycleBundles = [	{ CreditCycleId: 1, CreditPercentage: 1} ];
+		let mockSeason = { }
+
+		let mockCreditCycles = [ { CreditCycleId: 1} ] ;
+		let mockClientBundle = { BundleQuantity: 3 };
+		let mockBundle = { BundleCost_Fixed: 500,	BundleCost_PerBundle: 200 };
+		let mockBundleInputs = [
 				//					ID	SelectionGroup	FixedAdj	PerBundleAdj	ReqQtyFrom	ReqQtyTo	ReqInputId
 					new BundleInput(1, 	'Jon Snow', 	0, 			0, 				null, 		null, 		null)
-				],
-				CreditCycleBundles: [
-					{ CreditCycleId: 1, CreditPercentage: 1}
-				]
-			},
-			ClientBundleInputChoices: [],
-		}
-		let mockSeason = {	CreditCycles: [	{ CreditCycleId: 1}	] };
+		];
+		let	mockClientBundleInputChoices = [];
 		
+		spyOn(mockWhere,'where').and.returnValues(mockCreditCycles,mockBundle,mockBundleInputs,mockCreditCycleBundles,mockClientBundleInputChoices)
 		expect(creditCalculationService.calculateClientBundleCredit(mockClientBundle, mockSeason)).toEqual(1100);
 								
 	})
 
 	it('calculates simple multi-input bundle price correctly', function() {
-		let mockClientBundle = {
-			BundleQuantity: 2,
-			Bundle: {
-				BundleCost_Fixed: 500,	BundleCost_PerBundle: 200,
-				BundleInputs: [
+		let mockCreditCycleBundles = [	{ CreditCycleId: 1, CreditPercentage: 1} ];
+		let mockSeason = { }
+
+		let mockCreditCycles = [ { CreditCycleId: 1} ] ;
+		let mockClientBundle = { BundleQuantity: 2 };
+		let mockBundle = { BundleCost_Fixed: 500,	BundleCost_PerBundle: 200 };
+		let mockBundleInputs = [
 				//					ID	SelectionGroup	FixedAdj	PerBundleAdj	ReqQtyFrom	ReqQtyTo	ReqInputId
 					new BundleInput(1, 	'Jon', 			20, 		0, 				null, 		null, 		null),
 					new BundleInput(2, 	'Cersei', 		0, 			10, 			null, 		null, 		null),
 					new BundleInput(3, 	'Arya', 		0, 			0, 				null, 		null, 		null),
-				],
-				CreditCycleBundles: [ { CreditCycleId: 1, CreditPercentage: 1}	]
-			},
-			ClientBundleInputChoices: [],
-		}
-		let mockSeason = {	CreditCycles: [	{ CreditCycleId: 1}	] };
+				];
+		let	mockClientBundleInputChoices = [];
 		
+		spyOn(mockWhere,'where').and.returnValues(mockCreditCycles,mockBundle,mockBundleInputs,mockCreditCycleBundles,mockClientBundleInputChoices)
 		expect(creditCalculationService.calculateClientBundleCredit(mockClientBundle, mockSeason)).toEqual(940);
 								
 	});
