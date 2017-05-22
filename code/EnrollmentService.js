43f6bde347fec23351185a46cae226043619b85e
Eric Griffith <eric@griffithnet.com>
2017-05-22 23:14:56 +0300

    make this match the changes for step-5 and step-6

--- code/EnrollmentService.js
+++ code/EnrollmentService.js
@@ -44,25 +44,25 @@
     		},
 
     		//Calculate the total credit of a bundle
-    		calculateClientBundleCredit: function (clientBundle, season) {
+    		calculateClientBundleCredit: function (clientBundle, season, cbics) {
+					// clientBundle is required.  cbics and season can be pulled from dataservice
+					// but we must allow for cbics to be passed so that pricing while in edit mode
+					// works
 
-    			//Bundle related costs
     			var bundleQuantity = clientBundle.BundleQuantity || 0;
-
-    			if (clientBundle.cbics) {
-    				//console.log(clientBundle);
-    			}
     			if (bundleQuantity === 0) return 0;
+
+					var cb = clientBundle
 
     			var creditCycles = ht.get('creditCycles').where(['DistrictId', 'SeasonId'], [season.DistrictId, season.SeasonId]);
 
-    			var cb = clientBundle
     			var bundle = ht.get('bundles').where(['DistrictId', 'BundleId'], [cb.DistrictId, cb.BundleId]);
-				if (!bundle) return 0;	 // when doing data entry
-				var bundleInputs = ht.get('bundleInputs').where(['DistrictId', 'BundleId'], [cb.DistrictId, cb.BundleId]);
-				var creditCycleBundles = ht.get('creditCycleBundles').where(['DistrictId', 'BundleId'], [cb.DistrictId, cb.BundleId]);
-				var clientBundleInputChoices = ht.get('clientBundleInputChoices').where(['DistrictId', 'ClientBundleId'], [cb.DistrictId, cb.ClientBundleId]);
-				
+					if (!bundle) return 0;	 // when doing data entry
+
+					var bundleInputs = ht.get('bundleInputs').where(['DistrictId', 'BundleId'], [cb.DistrictId, cb.BundleId]);
+					var creditCycleBundles = ht.get('creditCycleBundles').where(['DistrictId', 'BundleId'], [cb.DistrictId, cb.BundleId]);
+					var clientBundleInputChoices = cbics || ht.get('clientBundleInputChoices').where(['DistrictId', 'ClientBundleId'], [cb.DistrictId, cb.ClientBundleId]);
+
 
     			// Maps selection group names to the objects that contain
     			//	input: the Input Entity which will be used for pricing (none if there is none for this selectin group)
@@ -73,14 +73,12 @@
 
     			// First, add all unique selectionGroups to the map, with a count of inputs for each group
     			// (count>1 means that a ClientBundleInputChoice MUST be made)
-    			// TODO: 1.26 use new hashtables
     			angular.forEach(bundleInputs, function (bi) {
     				if (!selectionGroupMap[bi.SelectionGroup]) selectionGroupMap[bi.SelectionGroup] = { input: null, inputCount: 0 };
     				selectionGroupMap[bi.SelectionGroup].inputCount += 1;
     			});
 
     			// For all ClientBundleInputChoices, add the selected bundleInput to the map
-    			// TODO: 1.26 use new hashtables
     			angular.forEach(clientBundleInputChoices, function (cbic) {  // include all input choices
     				var bundleInput = ht.get('BundleInputs')
 							.where(['DistrictId', 'BundleInputId'], [cbic.DistrictId, cbic.BundleInputId])
@@ -93,9 +91,8 @@
     			// Collect the inputs requiring other inputs for a third iteration through
     			var inputsRequiringInputs = [];
 
-    			// Collect all the inputs that do NOT have a CBIC or Required input as long as they meet 
+    			// Collect all the inputs that do NOT have a CBIC or Required input as long as they meet
     			// Min/Max conditional requirements.  Lean input==null if it should not be included in pricing
-    			// TODO: 1.26 use new hashtables
     			angular.forEach(bundleInputs, function (bi) {
     				var sg = selectionGroupMap[bi.SelectionGroup];
     				if (bi.RequiredBundleInputId) {
@@ -115,8 +112,8 @@
     			});
 
     			// Now that all the other inputs are included, we can see if the inputs
-    			// requiring other inputs are to be included.  This must be run until no 
-    			// more required inputs are added because an added input might be required by 
+    			// requiring other inputs are to be included.  This must be run until no
+    			// more required inputs are added because an added input might be required by
     			// a previous input
     			do {
     				var remainingInputs = []; // to hold no-added inputs so the iterator is respected
@@ -144,7 +141,6 @@
 
     			// Calculate price
     			// ... get bundle costs
-    			// TODO: 1.26 use new hashtables
     			var bundleCost = 0;
     			var bundleCostFixed = bundle.BundleCost_Fixed || 0;
     			var bundleCostIncremental = bundle.BundleCost_PerBundle || 0;
@@ -166,28 +162,25 @@
                     });
 
     			//Adjust for credit cycle allocation percentage
-    			// TODO: 1.26 use new hashtables
     			var creditCycle = creditCycles[0];
     			var creditCycleBundle = $.grep(creditCycleBundles, function (ccb) { return ccb.CreditCycleId === creditCycle.CreditCycleId; })[0];
 
     			bundleCost = creditCycleBundle.CreditPercentage * bundleCost;
-
-
-    			//console.log(selectionGroupMap);
     			return bundleCost;
 
-    			//Given a client bundle, determine all the credit for it's inputs
-    			//variable cost per bundleinputs chosen
     		},
 
     	};
 
     	return functions;
 
-    	// Within the parent object, find the first-level child object that has the 
+    	// Within the parent object, find the first-level child object that has the
     	// passed property and value
     	// myObj = { foo: { name: 'a', id: 1}, bar: { name: 'b', id:2} }
     	// getChildObject(myObj, 'id', 1) // returns the child object foo
+    	//
+    	// this is used during the processing of inputsrequiringinputs to lookup a
+    	// selectiongroup that has resolved to a specific bundleinput id
     	function getChildObject(parentObject, childObjectProperty, childObjectValue) {
     		for (let prop in parentObject) {
     			let childObject = parentObject[prop];
@@ -196,8 +189,8 @@
     		return null;
     	}
 
-    	// Get a property value from a nexted object using the property path
-    	// myObject = { foo: { bar: 1, baz: 2} } 
+    	// Get a property value from a nested object using the property path
+    	// myObject = { foo: { bar: 1, baz: 2} }
     	// getValueFromPropertyPath(myObject,'foo.bar')	 // returns '1'
     	function getValueFromPropertyPath(object, path) {
     		let props = path.split('.');
